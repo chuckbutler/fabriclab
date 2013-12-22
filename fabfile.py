097e277d4231fc84d6db280cd7fc15b9593e09a4
Charles Butler <chuck@dasroot.net>
2013-12-22 01:29:03 -0500

    Changed nomenclature of functions to be more descriptive

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from fabric.api import env, run, sudo
+from fabric.api import env, run, sudo, local
 
 
 env.hosts = [ '10.0.2.158' ]
@@ -9,7 +9,7 @@ def upgrade_packages():
     sudo('apt-get update')
     sudo('apt-get upgrade -y')
 
-def bootstrap_user():
+def bootstrap_server():
     sudo('apt-get install vim git-core htop -y')
     run('git clone https://github.com/chuckbutler/dotfilesv2.git .dotfiles')
     run('ln -s .dotfiles/dotvim .vim')
@@ -17,4 +17,16 @@ def bootstrap_user():
     run('ln -s .dotfiles/gitconfig .gitconfig')
     run('ln -s .dotfiles/githelpers .githelpers')
 
-    
+def bootstrap_workstation():
+    local('sudo apt-get install vim git-core htop -y')
+    local('git clone https://github.com/chuckbutler/dotfilesv2.git $HOME/.dotfiles')
+    local('ln -s $HOME/.dotfiles/dotvim .vim')
+    local('ln -s $HOME/.dotfiles/dotvimrc .vimrc')
+    local('ln -s $HOME/.dotfiles/gitconfig .gitconfig')
+    local('ln -s $HOME/.dotfiles/githelpers .githelpers')
+
+
+def setup_qemu(user="charles"):
+    sudo('apt-get install qemu-kvm libvirt-bin bridge-utils -y')
+    sudo('adduser' + user + 'libvirtd')
+        
